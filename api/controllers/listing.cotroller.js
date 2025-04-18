a3f796d73890c761220ceaf5642f8a9254657877
mohtashim220 <mohtashim220ali@gmail.com>
2025-04-19 05:00:44 +0530

    update listing.controller.jsx

--- api/controllers/listing.cotroller.js
+++ api/controllers/listing.cotroller.js
@@ -73,15 +73,6 @@ export const getListings = async (req, res, next) => {
     const limit = parseInt(req.query.limit) || 9;
     const startIndex = parseInt(req.query.startIndex) || 0;
 
-     
-
-     
-
-     
-     
-
-     
-   
 
     const searchTerm = req.query.searchTerm || '';
     const sort = req.query.sort || 'createdAT';
