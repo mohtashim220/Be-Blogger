e80c908b8bc4728bab062ea7140d31565b6daf2f
mohtashim220 <mohtashim220ali@gmail.com>
2025-04-19 05:12:40 +0530

    updated listing.controller.js

--- api/controllers/listing.cotroller.js
+++ api/controllers/listing.cotroller.js
@@ -75,7 +75,7 @@ export const getListings = async (req, res, next) => {
 
 
     const searchTerm = req.query.searchTerm || '';
-    const sort = req.query.sort || 'createdAT';
+    const sort = req.query.sort || 'createdAt';
     const order = req.query.order || 'desc';
 
     const listings = await Listing.find({
