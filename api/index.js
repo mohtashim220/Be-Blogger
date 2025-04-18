dc05c2545fec6a5c1571f4b2b82829ddcf1d006d
mohtashim220 <145795470+mohtashim220@users.noreply.github.com>
2025-04-19 02:59:32 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,9 +5,9 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-const serverless = require('serverless-http');
+ 
 dotenv.config();
-module.exports.handler = serverless(app);
+ 
 
 mongoose
   .connect(process.env.MONGO, {
@@ -23,12 +23,7 @@ mongoose
   });
 
 const app = express();
-app.use(cors(
-  {
-    origin:["https://be-blogger-client.vercel.app"],
-    methods:["POST","GET"],
-    credentials:true
-  }
+ 
   ));
 
 
